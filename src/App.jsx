cb5084f2ccfef37ce47f3971d86fbf5663cec390
emmeiwhite <emmeiwhite@gmail.com>
2024-10-16 19:11:34 +0530

    Object & UI --- spread operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './../src/tutorial/01-useState/starter/03-useState-array'
+import Starter from './../src/tutorial/01-useState/starter/04-useState-object'
 function App() {
   return (
     <div className="container">
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,35 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: 'Rawdha Bejaoui',
+    age: 26,
+    profession: 'boutique'
+  })
 
-export default UseStateObject;
+  function handlePerson() {
+    setPerson({
+      ...person,
+      name: 'Imran & Rawdha',
+      age: '32 & 26',
+      profession: 'EveDress & EmmeiSoft'
+    })
+  }
+  return (
+    <div>
+      <h2>Person Details</h2>
+      <h3>Name: {person.name}</h3>
+      <h3>Age: {person.age}</h3>
+      <h3>Profession: {person.profession}</h3>
+
+      <button
+        className="btn"
+        onClick={handlePerson}
+      >
+        {person.name === 'Imran & Rawdha' ? 'Together Forever' : 'Update Person'}
+      </button>
+    </div>
+  )
+}
+
+export default UseStateObject
