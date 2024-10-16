2affc0d9616070fad8f4b1da5562e3ddeaf796ea
emmeiwhite <emmeiwhite@gmail.com>
2024-10-16 18:31:58 +0530

    Rendering list in JSX

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -2,7 +2,20 @@ import { data } from '../../../data'
 
 const UseStateArray = () => {
   console.log(data)
-  return <h2>useState array example</h2>
+  const peopleJSX = data.map(person => (
+    <Person
+      name={person.name}
+      key={person.id}
+    />
+  ))
+  return <div>{peopleJSX}</div>
 }
 
+function Person({ name }) {
+  return (
+    <div>
+      <h4>{name}</h4>
+    </div>
+  )
+}
 export default UseStateArray
