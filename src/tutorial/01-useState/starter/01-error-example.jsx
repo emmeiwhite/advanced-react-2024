e7a79aa1db38f9c9569cf0254ab9c599c040e7e8
emmeiwhite <emmeiwhite@gmail.com>
2024-10-16 15:12:37 +0530

    Now, the ErrorExample fxn is called again which means re-rendered and we can see correct data in UI & console.logs as well

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react'
+
 const ErrorExample = () => {
-  let count = 0
+  let count = 0 // Normal value
+  const stateArray = useState(0) // State value
+  console.log('state count ', stateArray[0])
 
   function handleCount() {
     count += 1
     console.log(count)
+    // only updating within the function, there is no  component level re-render - principle on which React works!
+  }
+
+  function handleCountWithState() {
+    stateArray[1](count => count + 1)
   }
 
   return (
     <div>
-      <h2>Count: {count}</h2>
+      {/* <h2>Count: {count}</h2> */}
+      <h2>Count: {stateArray[0]}</h2>
       <button
         onClick={handleCount}
         className="btn"
       >
         Click me
+      </button>
+      &nbsp;&nbsp;&nbsp;
+      <button
+        onClick={handleCountWithState}
+        className="btn"
+      >
+        update state
       </button>
     </div>
   )
