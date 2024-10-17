add68c5930f340f572347e5e8086704f0d76f896
emmeiwhite <emmeiwhite@gmail.com>
2024-10-17 15:03:38 +0530

    Successfully fetched the API data and rendered in UI with React state and Declarative syntax

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -9,6 +9,28 @@ const FetchData = () => {
   const [loading, setLoading] = useState(true)
   const [error, setError] = ''
 
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch(url)
+      if (!response.ok) {
+        throw new Error('Could not fetch data')
+      }
+      const data = await response.json()
+
+      console.log(data)
+
+      setLoading(false)
+      setUsers(data)
+    } catch (error) {
+      setError(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
   if (loading) {
     return <p>loading ...</p>
   }
@@ -17,16 +39,29 @@ const FetchData = () => {
     return <p>{error}</p>
   }
 
-  const fetchUsers = async () => {
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
-  }
+  return (
+    <section className="container">
+      {users.map(user => (
+        <User
+          {...user}
+          key={user.id}
+        />
+      ))}
+    </section>
+  )
+}
 
-  useEffect(() => {
-    fetchUsers()
-  }, [])
-
-  return <h2>fetch data example</h2>
+function User({ id, login, avatar_url }) {
+  return (
+    <div className="user">
+      <h3>Github Users</h3>
+      <img
+        src={avatar_url}
+        alt={login}
+        className="img"
+      />
+      <h3>{login}</h3>
+    </div>
+  )
 }
 export default FetchData
