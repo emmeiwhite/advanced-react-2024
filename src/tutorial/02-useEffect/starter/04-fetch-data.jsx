6c51954bc4bece14e464b84f182cf24daa2116d7
emmeiwhite <emmeiwhite@gmail.com>
2024-10-17 14:26:43 +0530

    AJAX - fetch example with useEffect to perform side effects

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,32 @@
-const url = 'https://api.github.com/users';
+import { useEffect, useState } from 'react'
+
+const url = 'https://api.github.com/users'
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
-};
-export default FetchData;
+  // UI is the reflection of state, so let's describe the state
+
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = ''
+
+  if (loading) {
+    return <p>loading ...</p>
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
+
+  const fetchUsers = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  return <h2>fetch data example</h2>
+}
+export default FetchData
