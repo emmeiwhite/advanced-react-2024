2a42c17df469ec1921f591c3f8912ba6370cdba7
emmeiwhite <emmeiwhite@gmail.com>
2024-10-18 11:43:10 +0530

    Project Structure --- One folder per component with associated files like CSS etc and index.js where we import and export simultaneously

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Starter from './../src/tutorial/02-useEffect/starter/05-cleanup-function'
+import Navbar from './tutorial/04-project-structure/starter/Navbar'
 function App() {
   return (
     <div className="container">
-      <Starter />
+      <Navbar />
     </div>
   )
 }
---
+++ src/tutorial/04-project-structure/starter/Navbar/index.jsx
@@ -0,0 +1,3 @@
+export { default } from './Navbar'
+
+// This means import Navbar from './Navbar' & then export default Navbar. So two statements in one short-cut statement
