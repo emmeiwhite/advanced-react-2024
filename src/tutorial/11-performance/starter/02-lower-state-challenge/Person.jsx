bfd2d554b6f1b0eb417c43893e0ff4030eaae719
emmeiwhite <emmeiwhite@gmail.com>
2024-10-24 19:44:24 +0530

    Lowered the state again and made the more performant

--- src/tutorial/11-performance/starter/02-lower-state-challenge/Person.jsx
+++ src/tutorial/11-performance/starter/02-lower-state-challenge/Person.jsx
@@ -1,8 +1,9 @@
 const Person = ({ name }) => {
+  console.log('Person Rendered!')
   return (
     <div>
       <h4>{name}</h4>
     </div>
-  );
-};
-export default Person;
+  )
+}
+export default Person
