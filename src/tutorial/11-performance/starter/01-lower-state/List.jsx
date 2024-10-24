9160b3fa2f78b6b6cadbb183cbbc8748cea8249a
emmeiwhite <emmeiwhite@gmail.com>
2024-10-24 18:55:07 +0530

    state update due to btn click triggers re-render of the component, which re-renders other children Components within it

--- src/tutorial/11-performance/starter/01-lower-state/List.jsx
+++ src/tutorial/11-performance/starter/01-lower-state/List.jsx
@@ -1,12 +1,19 @@
-import Person from './Person';
+import Person from './Person'
 
 const List = ({ people }) => {
+  // When the parent component re-renders List also re-renders along with its child components!
+  // Too many re-renders are not good for performance if done for no purpose!
   return (
     <div>
-      {people.map((person) => {
-        return <Person key={person.id} {...person} />;
+      {people.map(person => {
+        return (
+          <Person
+            key={person.id}
+            {...person}
+          />
+        )
       })}
     </div>
-  );
-};
-export default List;
+  )
+}
+export default List
