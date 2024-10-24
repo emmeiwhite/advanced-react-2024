fe7e220f4e75bdc0f15f054f4c3a4865b626efd8
emmeiwhite <emmeiwhite@gmail.com>
2024-10-24 19:07:02 +0530

    Testing Performance in React Dev-Tools - Profiler!

--- src/tutorial/11-performance/starter/01-lower-state/index.jsx
+++ src/tutorial/11-performance/starter/01-lower-state/index.jsx
@@ -6,10 +6,19 @@ import Button from './Button'
 
 const LowerState = () => {
   const [people, setPeople] = useState(data)
+  const [count, setCount] = useState(0)
 
   return (
     <section>
-      <Button />
+      {/* <Button /> */}
+
+      <button
+        className="btn"
+        onClick={() => setCount(count + 1)}
+        style={{ marginBottom: '1rem' }}
+      >
+        count {count}
+      </button>
       <List people={people} />
     </section>
   )
